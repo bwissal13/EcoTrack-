f97e171218b28290e32b92d5076dddfdfdba472f
bwissal13 <baazizwissal13@gmail.com>
2024-12-17 11:51:31 +0100

    git commit -m "first commit"

---
+++ src/main/java/org/example/ecotrackbackend/EcoTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecotrackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTrackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoTrackBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ecotrackbackend/EcoTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ecotrackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTrackBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
